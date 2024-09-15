921df2319fd9af32f83be92a9f08f508c340b383
Junho Yoon <enffl95@icloud.com>
2024-09-15 18:49:37 +0900

    다이어트

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,13 +1,3 @@
-/**
- * Assets are folder located in the /lib/assets folder
- */
-//= link_directory ../../../lib/assets .js
-//= link_directory ../../../lib/assets .css
-//= link_tree ../../../lib/assets
-//= link_tree ../../../lib/assets/css
-//= link_tree ../../../lib/assets/js
-//= link_tree ../../../lib/assets/media
-//= link_tree ../../../lib/assets/plugins
-
 //= link_tree ../images
+//= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :init_theme_mode, :init_theme_direction, :init_layout
 
-  # Main initialization
-  def initialize
-    initThemeMode
-    initThemeDirection
-    initLayout
-  end
+  private
 
-  # Init theme mode option from settings
-  def initThemeMode
+  def init_theme_mode
     helpers.setModeSwitch(Rails.configuration.settings.KT_THEME_MODE_SWITCH_ENABLED)
     helpers.setModeDefault(Rails.configuration.settings.KT_THEME_MODE_DEFAULT)
   end
 
-  # Init theme direction option (RTL or LTR) from settings
-  # Init RTL html attributes by checking if RTL is enabled.
-  # This function is being called for the html tag
-  def initThemeDirection
+  def init_theme_direction
     helpers.setDirection(Rails.configuration.settings.KT_THEME_DIRECTION)
-
     if helpers.isRtlDirection
       helpers.addHtmlAttribute('html', 'direction', 'rtl')
       helpers.addHtmlAttribute('html', 'dir', 'rtl')
@@ -26,10 +17,8 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # Init layout html attributes and classes
-  def initLayout
+  def init_layout
     helpers.addHtmlAttribute('body', 'id', 'kt_app_body')
     helpers.addHtmlAttribute('body', 'data-kt-name', helpers.getName())
   end
-
 end
